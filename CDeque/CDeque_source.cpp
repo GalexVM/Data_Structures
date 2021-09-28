7ddd04c3c27b6fe77831009649a2495954e9d1f3
Gabriel Valdivia <vgabriel.gm37@gmail.com>
2021-09-28 00:20:41 -0500

    CDeque: push_back terminado

--- CDeque/CDeque_source.cpp
+++ CDeque/CDeque_source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+
+int mod(int a, int b);
+
 class CVector
 {
 public:
@@ -39,7 +42,9 @@ public:
         m_size = size;
         m_nvect = 1;
         m_nelemTotales = 0;
-        bg= end = m_size/2;
+        bg = end = centerController = m_size / 2;
+        vectController = 2;
+        indexController = -3;
         map = new CVector [m_size];
     }
     ~CDeque() {
@@ -48,30 +53,26 @@ public:
     int& operator[](int i) {
         return map[i / m_size][i%m_size];
     }
+  
+    void UpdateIndexes() {
+        vectController = (vectController == 0) ? 4 : vectController - 1;
+        indexController = (indexController == -4) ? 0 : indexController - 1;
+    }
     void addVectFront() {
         bg--;
+        centerController--;
     }
     void addVectBack() {
         end++;
     }
     void push_back(int x) {
         m_nelemTotales++;
-        if (m_nvect == 1) {
-            if (2 + (m_nelemTotales % m_size) - 1 == 5) {
-                m_nvect++;
-                addVectBack();
-            }
-            else {
-                map[2 + (m_nelemTotales / (m_size - 1))][2 + (m_nelemTotales-1 % m_size) ] = x;
-            }
+        if (mod(m_nelemTotales - 1 + indexController, m_size) == 0 && m_nelemTotales != 1) {
+            addVectBack();
+            m_nvect++;
         }
-        if (m_nvect > 1) {
-            if ((m_nelemTotales + 1) % m_size == 0 && m_nelemTotales != 5) {
-                m_nvect++;
-                addVectBack();
-            }
-            map[2 + ((m_nelemTotales + 1) / m_size)][((m_nelemTotales + 1) % m_size)] = x;
-        }
+        map[((m_nelemTotales - 1 + vectController) / m_size)+centerController][mod(m_nelemTotales - 1 + indexController, m_size)] = x;
+           
     }
     void print() {
         
@@ -93,21 +94,27 @@ private:
     int m_nvect;
     int m_nelemTotales;
     int bg, end;
+    int vectController, indexController, centerController;
 };
+
 
 int main() {
     CDeque deque1;
-    deque1.push_back(1);
-    deque1.push_back(2);
-    deque1.push_back(3);
-    deque1.push_back(4);
-    deque1.push_back(5);
-    deque1.push_back(6);
-    deque1.push_back(7);
-    deque1.push_back(8);
-    deque1.push_back(9);
-    deque1.push_back(10);
+    for (int i = 0; i < 13; i++) {
+        deque1.push_back(i);
+    }
+   
     deque1.print();
     return 0;
+}
+
+int mod(int a, int b) {
+    int q = a / b;
+    int	r = a - (q * b);
+    if (a < int(0)) {
+        int ar = r;
+        r = b + ar;
+    }
+    return r;
 }
 
